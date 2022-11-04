89d3174b2a836e5acea23b010f0236ff056e65c6
akuma-codeleo <leolinnuk@gmail.com>
2022-11-04 13:15:30 +0100

    Added map description

--- src/Resources/ResourceManager.cpp
+++ src/Resources/ResourceManager.cpp
@@ -330,6 +330,22 @@ bool ResourceManager::loadJSONResources(const std::string& JSONPath)
 		}
 	}
 
+	auto levelsIt = document.FindMember("levels");
+	if (levelsIt != document.MemberEnd())
+	{
+	 	for (const auto& currentLevel : levelsIt->value.GetArray())
+		{
+			const auto description = currentLevel["description"].GetArray();
+			std::vector<std::string> levelRows;
+			levelRows.reserve(description.Size());
+			for (const auto& currentRow : description)
+			{
+				levelRows.emplace_back(currentRow.GetString());
+			}
+
+		}
+	}
+
 	return true;
 
 }
